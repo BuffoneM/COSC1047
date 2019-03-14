ed2fb8667fa7bc8fb843c82af04a107e078a4e1f
Michael Buffone <BuffoneM@users.noreply.github.com>
2019-03-14 19:23:45 -0400

    Assignment 7

---
+++ Assignment 7/src/PartA1.java
@@ -0,0 +1,18 @@
+/*
+ *	Michael Buffone 
+ * 	3/14/2019
+ * 	COSC1047W19
+ * 	Assignment 7 Custom Problem 1
+ * 
+ *  This program will generate a random quantity of numbers within the range
+ *  of -12250 to 13580 and put store them in a binary file. The program will
+ *  perform multiple tasks on the numbers generated.
+ */
+
+public class PartA1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
