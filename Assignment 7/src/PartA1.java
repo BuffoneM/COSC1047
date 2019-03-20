ba85c921c8711bd02f56c6f7ca4212cecb3ec598
Michael Buffone <BuffoneM@users.noreply.github.com>
2019-03-20 14:51:52 -0400

    Assignment 7

--- Assignment 7/src/PartA1.java
+++ Assignment 7/src/PartA1.java
@@ -15,23 +15,18 @@ public class PartA1 {
 	
 	public static void main(String[] args) {
 		
-		
 		File file = new File("MyData.dat");
 		
-		if(!file.exists()) {
-			populateFile(file);
-		}
+		populateFile(file);
 		readFile(file);
-		
-		
 		
 	}
 	
 	public static void populateFile(File file) {
-		//int randomQuantity = (int)(200 + (int)(Math.random() * (300 - 200 + 1)));
-		int randomQuantity = 1;
+		int randomQuantity = (int)(200 + (int)(Math.random() * (300 - 200 + 1)));
+		//int randomQuantity = 1;
 
-		try(DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
+		try(DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file, true)))) {
 			for(int i = 0; i < randomQuantity; i++) {
 				
 				int randomNum = (-12250 + (int)(Math.random() * (13580 - (-12250) + 1)));
@@ -54,14 +49,29 @@ public class PartA1 {
 		try(DataInputStream dos = new DataInputStream(new BufferedInputStream(new FileInputStream(file)))) {
 			
 			int count = 0;
+			int sum = 0;
+			int max = Integer.MIN_VALUE;
+			int min = Integer.MAX_VALUE;
+			
 			while(dos.available() > 0) {
-				System.out.printf("%1d ", dos.readInt());
-				if(++count == 15) {
-					count = 0;
-					System.out.println();
-				}
+				int currentNum = dos.readInt();
 				
+				// Calculations
+				count++;
+				sum += currentNum;
+				if(currentNum < min) {
+					min = currentNum;
+				}
+				if(currentNum > max) {
+					max = currentNum;
+				}
 			}
+			
+			System.out.println("The amount of integers is: " + count);
+			System.out.println("The sum of integers is: " + sum);
+			System.out.println("The average of integers is: " + (double)sum / (double)count);
+			System.out.println("The largest integer is: " + max);
+			System.out.println("The smallest integer is: " + min);
 			
 		}
 		catch(FileNotFoundException fnfe) {
