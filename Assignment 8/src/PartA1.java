40c8bff7bd4b26b0aa0af838681437bca8dac146
Michael Buffone <BuffoneM@users.noreply.github.com>
2019-03-26 19:03:11 -0400

    Assignment 8

---
+++ Assignment 8/src/PartA1.java
@@ -0,0 +1,18 @@
+/*
+ *	Michael Buffone 
+ * 	03/26/2019
+ * 	COSC1047W19
+ * 	Assignment 8 Custom Problem 1
+ * 
+ *  This program will recursively calculate a series and prove it using
+ *  a custom program with a for loop
+ */
+public class PartA1 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
