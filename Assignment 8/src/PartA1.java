70e460cfaf3817ab875a231a31aa845ecd2e3ed4
Michael Buffone <BuffoneM@users.noreply.github.com>
2019-03-27 16:14:47 -0400

    Update PartA1.java

--- Assignment 8/src/PartA1.java
+++ Assignment 8/src/PartA1.java
@@ -7,12 +7,38 @@
  *  This program will recursively calculate a series and prove it using
  *  a custom program with a for loop
  */
+
+import java.util.Scanner;
+
 public class PartA1 {
 
 	public static void main(String[] args) {
 
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int userNum = in.nextInt();
 		
+		System.out.println("Using recursion: " + recursiveSeries(userNum));
+		System.out.println("Using a for loop: " + loopSeries(userNum));
 		
 	}
+	
+	public static double recursiveSeries(double i) {
+		// Base case
+		if(i <= 1) return 1;
+		
+		// Return the current added to the subordinate
+		return ((2 * i) / (3 * i - 1)) + recursiveSeries(i - 1);
+
+	}
+	
+	public static double loopSeries(int userNum) {
+		double answer = 0;
+		for(int i = userNum; i >= 1; i--) {
+			answer += ((double)(2 * i) / (double)(3 * i - 1));
+		}
+		return answer;
+	}
+	
 
 }
